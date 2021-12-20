7ac8c3b4251214d22be9877e287003ed159a0de5
Wallace Yuen <wallace.yuen@otoy.com>
2021-12-20 16:47:28 +1300

    Update camera.h

--- oglrenderer/src/camera.h
+++ oglrenderer/src/camera.h
@@ -25,26 +25,26 @@ public:
         const float deltaX,
         float       deltaY)
     {
-        // Get the homogenous position of the camera and pivot point
+        // get the homogenous position of the camera and pivot point
         glm::vec4 position(mEye.x, mEye.y, mEye.z, 1);
         glm::vec4 pivot(mTarget.x, mTarget.y, mTarget.z, 1);
 
         float xAngle = deltaX;
         float yAngle = -deltaY;
 
-        // Extra step to handle the problem when the camera direction is the same as the up vector
+        // extra step to handle the problem when the camera direction is the same as the up vector
         float cosAngle = dot(normalize(mTarget - mEye), normalize(mUp));
         if (cosAngle * glm::sign(yAngle) > 0.99f)
         {
             yAngle = 0;
         }
 
-        // step 2: Rotate the camera around the pivot point on the first axis.
+        // rotate the camera around the pivot point on the first axis.
         glm::mat4x4 rotationMatrixX(1.0f);
         rotationMatrixX = glm::rotate(rotationMatrixX, xAngle, mUp);
         position = (rotationMatrixX * (position - pivot)) + pivot;
 
-        // step 3: Rotate the camera around the pivot point on the second axis.
+        // rotate the camera around the pivot point on the second axis.
         glm::mat4x4 rotationMatrixY(1.0f);
         glm::vec3 rightVector = normalize(cross(normalize(mTarget - mEye), normalize(mUp)));
         rotationMatrixY = glm::rotate(rotationMatrixY, yAngle, rightVector);
