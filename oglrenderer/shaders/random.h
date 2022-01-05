daab22db945c8fe2ad28ef83b35e421a1adc7509
Wallace Yuen <wallace.yuen@otoy.com>
2022-01-05 13:23:53 +1300

    Splitted random number from precomputefresnel

--- oglrenderer/shaders/random.h
+++ oglrenderer/shaders/random.h
@@ -1,7 +1,7 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
-float random(const vec2 st)
+float random1(const vec2 st)
 {
     return fract(sin(dot(st.xy,
         vec2(12.9898, 78.233))) *
@@ -22,5 +22,42 @@ vec3 random3(const vec3 p)
         dot(p, vec3(39.346f, 11.135f, 83.155f)),
         dot(p, vec3(73.156f, 52.235f, 09.151f)))) * 43758.5453f);
 }
+
+
+// A single iteration of Bob Jenkins' One-At-A-Time hashing algorithm.
+uint hash(uint x) {
+    x += (x << 10u);
+    x ^= (x >> 6u);
+    x += (x << 3u);
+    x ^= (x >> 11u);
+    x += (x << 15u);
+    return x;
+}
+
+// Compound versions of the hashing algorithm I whipped together.
+uint hash(uvec2 v) { return hash(v.x ^ hash(v.y)); }
+uint hash(uvec3 v) { return hash(v.x ^ hash(v.y) ^ hash(v.z)); }
+uint hash(uvec4 v) { return hash(v.x ^ hash(v.y) ^ hash(v.z) ^ hash(v.w)); }
+
+
+// Construct a float with half-open range [0:1] using low 23 bits.
+// All zeroes yields 0.0, all ones yields the next smallest representable value below 1.0.
+float floatConstruct(uint m) {
+    const uint ieeeMantissa = 0x007FFFFFu; // binary32 mantissa bitmask
+    const uint ieeeOne = 0x3F800000u; // 1.0 in IEEE binary32
+
+    m &= ieeeMantissa;                     // Keep only mantissa bits (fractional part)
+    m |= ieeeOne;                          // Add fractional part to 1.0
+
+    float  f = uintBitsToFloat(m);       // Range [1:2]
+    return f - 1.0;                        // Range [0:1]
+}
+
+// Pseudo-random value in half-open range [0:1].
+float random(float x) { return floatConstruct(hash(floatBitsToUint(x))); }
+float random(vec2  v) { return floatConstruct(hash(floatBitsToUint(v))); }
+float random(vec3  v) { return floatConstruct(hash(floatBitsToUint(v))); }
+float random(vec4  v) { return floatConstruct(hash(floatBitsToUint(v))); }
+
 
 #endif
